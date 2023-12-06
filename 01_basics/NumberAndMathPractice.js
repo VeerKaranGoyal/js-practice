5f0423bde4046a2db6a326037b72b0a4bb53b18d
VeerKaranGoyal <140520822+VeerKaranGoyal@users.noreply.github.com>
2023-12-06 05:25:26 +0000

    NumberAndMatch

---
+++ 01_basics/NumberAndMathPractice.js
@@ -0,0 +1,10 @@
+// primitive type definition and object type definition
+let number1 = 10323345.234
+let number2 = new Number(11.5)
+
+console.log(number1.toPrecision(2))
+console.log(number1.toFixed(2))
+console.log(number1.toLocaleString('en-IN'))
+console.log(number1.toExponential(2))
+
+// *************** Math Operations ************************
