0ae0d5be2bb04f82c0edcb49cc61f21b739453da
VeerKaranGoyal <140520822+VeerKaranGoyal@users.noreply.github.com>
2023-12-06 04:23:21 +0000

    String Practice

---
+++ 01_basics/stringsPractice.js
@@ -0,0 +1,24 @@
+// String: Primitive definition and Object definition
+let string1 = "Karan Goyal"
+let string2 = new String("Veer Karan")
+// console.log(string1)
+// console.log(string2)
+
+// console.log(typeof string1)
+// console.log(typeof string2)
+// console.log(string2.length)
+// console.log(string1.length)
+
+// console.log(string2.split(' '))
+// console.log(string1.split(' '))
+// console.log(string2.includes('Karan'))
+// console.log(string2.includes('xyz'))
+
+console.log(string1+string2)
+console.log(string2.indexOf('Z'))
+console.log(string2.charAt(0))
+
+
+
+
+
